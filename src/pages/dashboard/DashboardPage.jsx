331ea710e783936b3ad5dfe4487db31e7aae9640
juz2019076 <juz-2019076@kinal.edu.gt>
2024-05-14 08:14:16 -0600

    Se agrega el dashboard

--- src/pages/dashboard/DashboardPage.jsx
+++ src/pages/dashboard/DashboardPage.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import api from '../../services/api';
+import Blog from '../../components/blog/Blog';
 
-export const DashboardPage = () => {
+const DashboardPage = () => {
+  const [publications, setPublications] = useState([]);
+
+  useEffect(() => {
+    const fetchPublications = async () => {
+      try {
+        const response = await api.get('/publications');
+        setPublications(response.data);
+      } catch (error) {
+        console.error('Error fetching publications', error);
+      }
+    };
+
+    fetchPublications();
+  }, []);
+
   return (
-    <div>DashboardPage</div>
-  )
-}
+    <div>
+      <h1>Dashboard</h1>
+      <Blog publications={publications} />
+    </div>
+  );
+};
+
+export default DashboardPage;
